fe207a49e44e77a980bbc21aec07684ad6f56be6
Tomas Sykora <tsykora@redhat.com>
2014-11-18 18:42:47 +0100

    added javax.cache cache-api 1.0.0-PFD

--- rmBuildDeploy.sh
+++ rmBuildDeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm ~/programs/servers/eap6.2.1-forJONlib/jboss-eap-6.2/standalone/deployments/ispnweb.war
-mvn -s /home/tsykora/programs/eclipseWorkspace/settings_mead_jdg_plus_local.xml clean package -Dmaven.test.skip=true
-cp target/ispnweb.war ~/programs/servers/eap6.2.1-forJONlib/jboss-eap-6.2/standalone/deployments/
+rm ~/servers/eap/jboss-eap-6.3/standalone/deployments/ispnweb.war
+mvn clean package -Dmaven.test.skip=true
+cp target/ispnweb.war ~/servers/eap/jboss-eap-6.3/standalone/deployments/
