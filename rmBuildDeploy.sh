578a2c3c20ae5ac3a499341923b30a8ba9a00bb9
Tomas Sykora <tsykora@redhat.com>
2015-06-10 09:16:29 +0200

    bump version to 6.3.0.Final-redhat-3

--- rmBuildDeploy.sh
+++ rmBuildDeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm ~/servers/eap/jboss-eap-6.3/standalone/deployments/ispnweb.war
-mvn clean package -Dmaven.test.skip=true
-cp target/ispnweb.war ~/servers/eap/jboss-eap-6.3/standalone/deployments/
+rm ~/servers/eap/jboss-eap-6.4-forJON/standalone/deployments/ispnweb.war 
+mvn clean package -Dmaven.test.skip=true 
+cp target/ispnweb.war ~/servers/eap/jboss-eap-6.4-forJON/standalone/deployments/
